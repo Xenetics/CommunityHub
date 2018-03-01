299a1552e5fa8724eaafa32e6eec614089fa47da
xenetics <r.angus.oconnor@gmail.com>
2018-03-01 09:52:09 -0500

    partial push art & anim

---
+++ Assets/Animations/UI/ContentPanelAnimEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentPanelAnimEvents : MonoBehaviour
+{
+    public void ProxySetState()
+    {
+        GameManager.Instance.SetState(UIManager.Instance.NextState);
+    }
+}
